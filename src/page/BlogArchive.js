838ec8a2b58c0e1e3dacd663c7fc4fd904b22f77
Abdulrahman Sesay <abdulrahmansesay.sl@gmail.com>
2023-02-14 19:23:10 -0800

    I do a significant changes in the index,about,blog,contact,footer,hero,and project.js components.

--- src/page/BlogArchive.js
+++ src/page/BlogArchive.js
@@ -18,7 +18,7 @@ const BlogArchive = () => {
             Blogs
           </h2>
 
-          <p className="pb-5">Some of my best blogs.</p>
+          <p className="pb-5">Some of my best blogs you can check on.</p>
         </div>
 
         <div></div>
